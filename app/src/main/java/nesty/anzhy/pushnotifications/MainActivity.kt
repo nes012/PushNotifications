d063b202438c2f628619af300c9545bbb58fc3ac
anzhy.diz <anzhy.nesty@gmail.com>
2022-06-22 21:24:46 -0400

    create basic notification

--- app/src/main/java/nesty/anzhy/pushnotifications/MainActivity.kt
+++ app/src/main/java/nesty/anzhy/pushnotifications/MainActivity.kt
@@ -1,5 +1,10 @@
 package nesty.anzhy.pushnotifications
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.graphics.BitmapFactory
 import android.os.Build
 import android.os.Bundle
@@ -17,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var notificationManagerCompat: NotificationManagerCompat
     private lateinit var builder: NotificationCompat.Builder
 
+    @SuppressLint("UnspecifiedImmutableFlag")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,8 +30,24 @@ class MainActivity : AppCompatActivity() {
         cookies = findViewById(R.id.cookies)
         buyBtn = findViewById(R.id.btnBuy)
 
+        // Create notification channel if device is using API 26+
+        createNotificationChannel()
+        var intent = Intent(this@MainActivity, SecondActivity::class.java)
+
         buyBtn.setOnClickListener {
             val numberOfCookies = cookies.text.toString()
+
+            var pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                PendingIntent.getActivity(
+                    this@MainActivity,
+                    0,
+                    intent,
+                    PendingIntent.FLAG_MUTABLE
+                )
+            } else {
+                PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+            }
+
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 builder = NotificationCompat.Builder(this, CHANNEL_ID).setContentTitle(
@@ -37,12 +59,31 @@ class MainActivity : AppCompatActivity() {
                             this.resources, R.drawable
                                 .chihuahua2
                         )
-                    ).setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    )
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             }
             notificationManagerCompat = NotificationManagerCompat.from(this@MainActivity)
             // Notification ID is unique for each notification you create
             notificationManagerCompat.notify(1, builder.build())
 
+        }
+    }
+
+    private fun createNotificationChannel() {
+        // Create Notification Channel only on API Level 26+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "My Channel Name"
+            val description = "My Channel description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = description
+
+            //Register the channel with the system
+            val manager: NotificationManager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(channel)
         }
     }
 }
---
+++ app/src/main/java/nesty/anzhy/pushnotifications/SecondActivity.kt
@@ -0,0 +1,31 @@
+package nesty.anzhy.pushnotifications
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import nesty.anzhy.pushnotifications.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
